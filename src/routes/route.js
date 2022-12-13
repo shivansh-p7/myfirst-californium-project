49e095038ca7f0fb4e7fda96a8650b0a2dcbb919
shivansh-p7 <shivanshsharma55364@gmail.com>
2022-12-13 17:36:39 +0530

    missing number problems

--- src/routes/route.js
+++ src/routes/route.js
@@ -39,7 +39,7 @@ router.get('/students', function (req, res){
 })
 
 // PATH Param example
-router.get('/student-details/:name', function(req, res){
+//router.get('/student-details/:name', function(req, res){
     /*
     params is an attribute inside request that contains 
     dynamic values.
@@ -48,101 +48,129 @@ router.get('/student-details/:name', function(req, res){
     and value is what is sent in the request
     */
 
-    let requestParams = req.params
+//     let requestParams = req.params
 
-    // JSON strigify function helps to print an entire object
-    // We can use many ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
+//     // JSON strigify function helps to print an entire object
+//     // We can use many ways to print an object in Javascript, JSON stringify is one of them
+//     console.log("This is the request "+ JSON.stringify(requestParams))
+//     let studentName = requestParams.name
+//     console.log('Name of the student is ', studentName)
     
-    res.send('Dummy response')
-})
+//     res.send('Dummy response')
+// })
 
-// PATH Param example
-router.get("/profile/:name", function(req, res){
-    console.log('Printing the request to find out wjere name is stored',req.params)
-    console.log('user name is',req.params.name)
-    //console.log(`User requesting for profile is ${name}`)
-    res.send("dummy details")
-})
+// // PATH Param example
+// router.get("/profile/:name", function(req, res){
+//     console.log('Printing the request to find out wjere name is stored',req.params)
+//     console.log('user name is',req.params.name)
+//     //console.log(`User requesting for profile is ${name}`)
+//     res.send("dummy details")
+// })
 
-// Query Param example
-router.get("/shoes", function(req, res){
-    console.log("The filter options for shoes are -",req.query)
-    //req.query.size
-    //req.query.brand
+// // Query Param example
+// router.get("/shoes", function(req, res){
+//     console.log("The filter options for shoes are -",req.query)
+//     //req.query.size
+//     //req.query.brand
     
 
 
 
-    res.send("no such shoes")
-})
-//1 Create an API for GET /movies that returns a list of movies. Define an array of movies in your code and return the value in response.
-router.get("/movies",function(req,res){
-    const favMovies=["Parasite","Martians","Holiday","Taree Zammen Par",];
-    res.send(favMovies)
-});
-//2Create an API GET /movies/:indexNumber (For example GET /movies/1 is a valid request and it should return
-// the movie in your array at index 1). You can define an array of movies again in your api
-//3 handle edge cases
-router.get("/movies/:indexNumber",function(req,res){
-    const favMovies=["Parasite","Martians","Holiday","Taree Zammen Par",];
+//     res.send("no such shoes")
+// })
+// //1 Create an API for GET /movies that returns a list of movies. Define an array of movies in your code and return the value in response.
+// router.get("/movies",function(req,res){
+//     const favMovies=["Parasite","Martians","Holiday","Taree Zammen Par",];
+//     res.send(favMovies)
+// });
+// //2Create an API GET /movies/:indexNumber (For example GET /movies/1 is a valid request and it should return
+// // the movie in your array at index 1). You can define an array of movies again in your api
+// //3 handle edge cases
+// router.get("/movies/:indexNumber",function(req,res){
+//     const favMovies=["Parasite","Martians","Holiday","Taree Zammen Par",];
    
-    res.send(favMovies[req.params.indexNumber] || "Use a valid index Number")
-});
-//Write another api called GET /films. Instead of an array of strings define an array of movie objects this time.
-// Each movie object should have values - id, name
-//return whole object in return
-router.get("/films",function(req,res){
-    const favFilms=[ {
-        id:1,
-        film:"Narnia"
-    },
-    {
-        id:2,
-        film:"HarryPotter"
-    },
-    {
-        id:3,
-        film:"Boys over Flowers"
-    },
-    {
-        id:4,
-        film:"ChillarParty"
-    },
-    {
-        id:5,
-        film:"DeadEnd"
-    }]
+//     res.send(favMovies[req.params.indexNumber] || "Use a valid index Number")
+// });
+// //Write another api called GET /films. Instead of an array of strings define an array of movie objects this time.
+// // Each movie object should have values - id, name
+// //return whole object in return
+// router.get("/films",function(req,res){
+//     const favFilms=[ {
+//         id:1,
+//         film:"Narnia"
+//     },
+//     {
+//         id:2,
+//         film:"HarryPotter"
+//     },
+//     {
+//         id:3,
+//         film:"Boys over Flowers"
+//     },
+//     {
+//         id:4,
+//         film:"ChillarParty"
+//     },
+//     {
+//         id:5,
+//         film:"DeadEnd"
+//     }]
        
-    res.send(favFilms)
-});
-//5Write api GET /films/:filmId where filmId is the value received in request path params. Use this value to return a movie object with this id.
-// In case there is no such movie present in the array, return a suitable message in the response body.
+//     res.send(favFilms)
+// });
+// //5Write api GET /films/:filmId where filmId is the value received in request path params. Use this value to return a movie object with this id.
+// // In case there is no such movie present in the array, return a suitable message in the response body.
 
-router.get("/films/:id",function(req,res){
-    const favFilms=[ {
-        id:1,
-        film:"Narnia"
-    },
-    {
-        id:2,
-        film:"HarryPotter"
-    },
-    {
-        id:3,
-        film:"Boys over Flowers"
-    },
-    {
-        id:4,
-        film:"ChillarParty"
-    },
-    {
-        id:5,
-        film:"DeadEnd"
-    }]
-       
-    res.send(favFilms[req.params.id] ||"no such exist with this id")
+// router.get("/films/:filmId",function(req,res){
+//     const favFilms=[ {
+//         id:1,
+//         film:"Narnia"
+//     },
+//     {
+//         id:2,
+//         film:"HarryPotter"
+//     },
+//     {
+//         id:3,
+//         film:"Boys over Flowers"
+//     },
+//     {
+//         id:4,
+//         film:"ChillarParty"
+//     },
+//     {
+//         id:5,
+//         film:"DeadEnd"
+//     }]
+//     for(let i=0;i<favFilms.length;i++){
+//         if( favFilms[i].id==req.params.filmId){
+//             res.send(favFilms[i].film)
+//         }
+//            }
+//            res.send("no such film exist with this id")
+    
+// })
+
+
+router.get("/sol1",function(req,res){
+    const numbers=[1,2,3,4,5,7,8,9];
+    let missingNumber= [];
+    for(let i=0;i<numbers.length;i++){
+        if(numbers[i+1]-numbers[i]===2){
+            missingNumber.push(numbers[i]+1)
+        }
+    }
+res.send({data: missingNumber})
+})
+
+router.get("/sol2",function(req,res){
+    const numbers=[33,34,35,37,38,39,40];
+    let missingNumber= 0;
+    for(let i=0;i<numbers.length;i++){
+        if(numbers[i+1]-numbers[i]===2){
+            missingNumber=numbers[i]+1
+        }
+    }
+res.send({data: missingNumber})
 })
 module.exports = router;
