793f6a66c576150ab40b4d6daa7113c89718b7d1
shivansh-p7 <shivanshsharma55364@gmail.com>
2022-12-15 21:21:30 +0530

    exta changes

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const app = express();
+const mongoose=require('mongoose')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//connecting nodejs app with mongoDb
+mongoose.connect("mongodb+srv://shivanshsharma:76Xjx6fMmlcP51HZ@shivansh-p7.zwfahec.mongodb.net/shivanshsharma", {useNewUrlParser: true})
+    .then(() => console.log('mongodb running on 27017'))
+    .catch(err => console.log(err))
+
+
+
+
+
 app.use('/', route);
+
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
